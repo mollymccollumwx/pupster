6f5f24ec749497cbd69b632a982ceb49cd5bd701
Molly <mollymccollumwx@gmail.com>
2021-02-12 15:56:07 -0500

    added logic for 20% like chance

--- src/containers/Discover/Discover.jsx
+++ src/containers/Discover/Discover.jsx
@@ -7,11 +7,14 @@ class Discover extends Component {
 
 state = {
   dogImageUrl: "",
+  dogsThatLikeYou: 0,
+  dogsThatYouLike: 0
 };
 
 componentDidMount(){
     this.displayDog();
 };
+
 
 displayDog = () => {
   const BASEURL = "https://dog.ceo/api/breeds/image/random";
@@ -20,6 +23,19 @@ displayDog = () => {
   });
 };
 
+handleLike = () => {
+    const randomNum = Math.floor(Math.random()*5);
+
+    if (randomNum === 4){
+        this.setState({ dogsThatLikeYou: this.state.dogsThatLikeYou + 1});
+    }
+    this.setState({ dogsThatYouLike: this.state.dogsThatYouLike + 1});
+    this.displayDog();
+};
+
+handleUnlike = () => {
+    this.displayDog();
+}
   render() {
     return (
       <div className="container">
@@ -36,11 +52,13 @@ displayDog = () => {
         <div className="row">
           <div className="col-sm-3"></div>
           <div className="col-sm-3 d-flex justify-content-start">
-            <button className="btn btn-danger">Dislike</button>
+            <button className="btn btn-danger" onClick={this.handleUnlike}>Dislike</button>
           </div>
           <div className="col-sm-3 d-flex justify-content-end">
-            <button className="btn btn-success">Like</button>
+            <button className="btn btn-success" onClick={this.handleLike}>Like</button>
           </div>
+          <h5>Dogs that like you: {this.state.dogsThatLikeYou} </h5>
+          <h5>Dogs that you've liked: {this.state.dogsThatYouLike} </h5>
         </div>
       </div>
     );
