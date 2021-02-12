1d29861c3ece821d88d4dca60334b8c4d68fa13a
Molly <mollymccollumwx@gmail.com>
2021-02-12 15:47:52 -0500

    added DOG API call with axios

--- src/containers/Discover/Discover.jsx
+++ src/containers/Discover/Discover.jsx
@@ -1,7 +1,25 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+
+
 class Discover extends Component {
+
+state = {
+  dogImageUrl: "",
+};
+
+componentDidMount(){
+    this.displayDog();
+};
+
+displayDog = () => {
+  const BASEURL = "https://dog.ceo/api/breeds/image/random";
+  axios.get(BASEURL).then((res) => {
+    this.setState({ dogImageUrl: res.data.message });
+  });
+};
+
   render() {
     return (
       <div className="container">
@@ -10,21 +28,18 @@ class Discover extends Component {
           <div className="col d-flex justify-content-center">
             <img
               alt="a very good boy"
-              src="https://images.dog.ceo/breeds/cockapoo/Scout.jpg" style={{height: 400}}
+              src={this.state.dogImageUrl}
+              style={{ height: 400 }}
             />
           </div>
         </div>
         <div className="row">
-            <div className="col-sm-3"></div>
+          <div className="col-sm-3"></div>
           <div className="col-sm-3 d-flex justify-content-start">
-            <button className="btn btn-danger">
-              Dislike
-            </button>
+            <button className="btn btn-danger">Dislike</button>
           </div>
           <div className="col-sm-3 d-flex justify-content-end">
-            <button className="btn btn-success">
-              Like
-            </button>
+            <button className="btn btn-success">Like</button>
           </div>
         </div>
       </div>
