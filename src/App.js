88168e4414e010ac46f458ebeaf809289bb37e1f
Molly <mollymccollumwx@gmail.com>
2021-02-12 15:23:19 -0500

    added navbar to the app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 import About from "./containers/About/About";
 import Search from "./containers/Search/Search";
 import Discover from "./containers/Discover/Discover";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+      <NavBar />
         <Route exact path="/" component={About}/>
         <Route exact path="/discover" component={Discover}/>
         <Route exact path="/search" component={Search}/>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-        <div>
-        <Link to="/about">About</Link>
-        <Link to="/discover">Discover</Link>
-        <Link to="/search">Search</Link>
-        </div>
-    );
+  return (
+    <div>
+      <Link to="/about">About</Link>
+      <Link to="/discover">Discover</Link>
+      <Link to="/search">Search</Link>
+    </div>
+  );
 };
 
 export default NavBar;
