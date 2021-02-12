54f4a9004afaf6485ac807ba9b68ca4990fb70f3
Molly <mollymccollumwx@gmail.com>
2021-02-12 15:18:28 -0500

    added Links to navbar component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import About from "./containers/About/About";
+import Search from "./containers/Search/Search";
+import Discover from "./containers/Discover/Discover";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route exact path="/" component={About}/>
+        <Route exact path="/discover" component={Discover}/>
+        <Route exact path="/search" component={Search}/>
+      </Router>
     </div>
   );
 }
